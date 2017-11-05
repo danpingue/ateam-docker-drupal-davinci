d8e25f8149d893467e2867e6d838100b157fc33f
ocastano <ocascal@gmail.com>
2017-11-05 13:45:21 +0100

    Redefine properties

--- configs/create-drupal-project.sh
+++ configs/create-drupal-project.sh
@@ -9,6 +9,7 @@ drush si -y standard --db-url=mysql://root:$MYSQL_ROOT_PASSWORD@$MYSQL_HOST:3306
 
 cd /var/www/$DRUPAL_PROJECT/
 composer require drupal/ctools
+composer require drupal/admin_toolbar
 composer require drupal/da_vinci
 composer require drupal/geolocation
 composer require drupal/panels
@@ -17,6 +18,7 @@ composer require drupal/paragraphs
 cd /var/www/$DRUPAL_PROJECT/web
 
 drush en ctools -y
+drush en admin_toolbar -y
 drush en da_vinci -y
 drush en geolocation -y
 drush en panels -y
--- configs/create-user-drupal-project.sh
+++ configs/create-user-drupal-project.sh
@@ -9,26 +9,20 @@ composer create-project drupal-composer/drupal-project:$DRUPAL_VERSION_MAYOR.x-d
 
 cd $DRUPAL_PROJECT/web
 drush si -y standard --db-url=mysql://root:$MYSQL_ROOT_PASSWORD@$MYSQL_HOST:3306/$DRUPAL_PROJECT --account-pass=admin 
+#  --locale=es 
 
 cd $DEVELOPER_HOME/$DRUPAL_PROJECT/
 composer require drupal/ctools
+composer require drupal/admin_toolbar
 composer require drupal/da_vinci
-composer require drupal/geolocation
-composer require drupal/panels
-composer require drupal/paragraphs
 
 cd $DEVELOPER_HOME/$DRUPAL_PROJECT/web
-
 drush en ctools -y
+drush en admin_toolbar -y
 drush en da_vinci -y
-drush en geolocation -y
-drush en panels -y
-drush en paragraphs -y
 
-drush cset system.theme default 'da_vinci' -y
-# drush cset system.theme default 'bartik' -y
+# drush cset system.theme default 'da_vinci' -y
 
-# chown -R www-data:www-data /var/www/
 sudo ln -s $DEVELOPER_HOME/$DRUPAL_PROJECT /var/www/$DRUPAL_PROJECT
 
 sudo chown -R www-data:www-data /var/www/
